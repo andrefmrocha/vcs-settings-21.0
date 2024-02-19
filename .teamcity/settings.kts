2f18ddf74ddc7283c9fead032238893470f906bf
admin <admin@TeamCity>
2024-02-19 16:22:53 +0000

    TeamCity change in 'Kubernetes Example' project: parameters of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -296,7 +296,6 @@ object Build : BuildType({
     params {
         param("teamcity.vault.set.env", "false")
         param("teamcity.vault.ssh.set.env", "true")
-        param("inheritedParamTest", "")
     }
 
     vcs {
