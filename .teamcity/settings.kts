6d6a02d0b2df58cd8f47da78db97e1e6ac9ce834
admin <admin@TeamCity>
2024-04-29 13:33:44 +0100

    TeamCity change in 'Kubernetes Example' project: project parameters were changed

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -59,6 +59,8 @@ project {
         param("teamcity.internal.telemetry.events.otlp.enabled", "true")
         param("teamcity.internal.executor.enabled", "true")
         param("teamcity.internal.telemetry.traces.internal.enabled", "true")
+        select("meow", "",
+                options = listOf("value", "meow", "bananas"))
         text("Regex", "regex",
               regex = "regex")
         param("system.meow", "meowww")
